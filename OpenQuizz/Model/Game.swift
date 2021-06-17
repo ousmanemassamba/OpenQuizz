45eaf0901d6ee595ec598e46cfadae189eac1c37
ousmane diouf <ousmane.diouf@expleogroup.com>
2021-06-17 13:33:53 +0200

    Init project

---
+++ OpenQuizz/Model/Game.swift
@@ -0,0 +1,63 @@
+//
+//  Game.swift
+//  OpenQuizz
+//
+//  Created by Ousmane Massamba DIOUF on 14/06/2021.
+//
+
+import Foundation
+
+class Game {
+    var score = 0
+    
+    private var questions = [Question]()
+    private var currentIndex = 0
+    
+    var state: State = .ongoing
+    
+    enum State {
+        case ongoing, over
+    }
+    
+    var currentQuestion: Question {
+        return questions[currentIndex]
+    }
+    
+    func refresh() {
+        score = 0
+        currentIndex = 0
+        state = .over
+        QuestionManager.shared.get { (questions) in
+            self.questions = questions
+            self.state = .ongoing
+            
+            let name = Notification.Name(rawValue: "QuestionsLoaded")
+            let notification = Notification(name: name)
+            NotificationCenter.default.post(notification)
+        }
+    }
+    private func receiveQuestions(_ questions: [Question]) {
+        self.questions = questions
+        print(questions)
+        state = .ongoing
+    }
+    
+    func answerCurrentQuestion(with answer: Bool) {
+        if (currentQuestion.isCorrect && answer) || (!currentQuestion.isCorrect && !answer) {
+            score += 1
+        }
+        goToNextQuestion()
+    }
+    
+    private func goToNextQuestion() {
+        if currentIndex < questions.count - 1 {
+            currentIndex += 1
+        } else {
+            finishGame()
+        }
+    }
+    
+    private func finishGame() {
+        state = .over
+    }
+}
---
+++ OpenQuizz/Model/Question.swift
@@ -0,0 +1,13 @@
+//
+//  Question.swift
+//  OpenQuizz
+//
+//  Created by Ousmane Massamba DIOUF on 14/06/2021.
+//
+
+import Foundation
+
+class Question {
+    var title = ""
+    var isCorrect = false
+}
